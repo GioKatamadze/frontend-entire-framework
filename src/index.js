f4f6021cf7b2252894bf16745948ff0a4b4b7e3d
GioKatamadze <giokat.inbox@gmail.com>
2023-03-21 20:45:55 +0400

    run app for specied apolloclient

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://test-task.entireframework.com/cms/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
