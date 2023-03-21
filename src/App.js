922f49b74b330a42095b6f3135c44acee4f30210
GioKatamadze <giokat.inbox@gmail.com>
2023-03-21 20:40:32 +0400

    import menu element in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Menu from "./menu.js";
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
 
 export default App;
